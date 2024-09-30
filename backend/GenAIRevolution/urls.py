bafde9181f999dc9b9faa64fff33be31d454502f
Tejas <tejasgulati11227@gmail.com>
2024-09-30 18:18:08 +0530

    Finals

--- backend/GenAIRevolution/urls.py
+++ backend/GenAIRevolution/urls.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView  # Add this import
+from django.views.generic import RedirectView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        # Add other API endpoints here
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
     path('api/', include('ai_models.urls')),
     path('api/users/', include('users.urls')),
-    # Add a root URL pattern that redirects to your API
     path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
 
